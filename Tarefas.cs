5cd803b94fa4b8c5ab78b71e2a80adfed4c443cf
Marcos WIlian <37801810+marcosWilian@users.noreply.github.com>
2019-06-18 21:59:37 -0300

    Create Tarefas.cs

---
+++ Tarefas.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConectandoServicoRest.Model
+{
+    public class Tarefas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+
+    }
+}
